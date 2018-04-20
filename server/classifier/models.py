6504533f9038ebc238f04a4675b609427ccd0c72
bienzaaron <ajbienz@purdue.edu>
2018-04-19 22:04:28 -0400

    add User and Classification models and database migrations

--- server/classifier/models.py
+++ server/classifier/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    pass
+
+class Classification(models.Model):
+    result = models.CharField(max_length=100)
+    confidence = models.CharField(max_length=100)
+    photo = models.ImageField(upload_to='img')
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+
--- server/whatisthis/settings.py
+++ server/whatisthis/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'classifier.apps.ClassifierConfig'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'whatisthis.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'classifier',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
@@ -99,6 +104,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'classifier.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
