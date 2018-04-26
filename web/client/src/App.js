95df0eedebea8a9d5e5aed4e4445c25cca549a99
Joel Kreider <kreiderj@purdue.edu>
2018-04-26 00:43:55 -0400

    temp commit for navbar

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
-import { Button } from 'react-bootstrap';
 import ImageUploader from 'react-image-uploader';
 import axios from 'axios';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import Nav from 'react-bootstrap/lib/Nav';
+import Navbar from 'react-bootstrap/lib/Navbar';
+import MenuItem from 'react-bootstrap/lib/MenuItem';
+import NavItem from 'react-bootstrap/lib/NavItem';
+import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+
 
 class App extends Component {
 
@@ -20,7 +26,8 @@ class App extends Component {
            this.onDrop = this.onDrop.bind(this);
            this.renderLoggedIn = this.renderLoggedIn.bind(this);
            this.renderDefault = this.renderDefault.bind(this);
-           this.handleClick = this.handleClick.bind(this);
+           this.handleLogin = this.handleLogin.bind(this);
+           this.handleRegister = this.handleRegister.bind(this);
   }
 
    onDrop(picture) {
@@ -32,24 +39,54 @@ class App extends Component {
     var apiBaseUrl = "http://167.99.228.85:8000/api/user/login/";
     var self = this;
     var parameters = {
-      "username" : this.state.username,
-      "password" : this.state.password
+      "username" : self.state.username,
+      "password" : self.state.password
     }
-    axios.post(apiBaseUrl, parameters);
+    const response = axios.post(apiBaseUrl, JSON.stringify(parameters), { headers: { 'Content-Type': 'application/json' } });
+    console.log(response);
+    this.setState({loggedIn:true})
+    ReactDOM.render(<App />, document.getElementById('root'));
   }
-  renderRegister(){
 
+  handleRegister(event){
+    var apiBaseUrl = "http://167.99.228.85:8000/api/user/login/";
+    var self = this;
+    var parameters = {
+      "username" : self.state.username,
+      "password" : self.state.password
+    }
+  
+    const response = axios.post(apiBaseUrl, JSON.stringify(parameters), { headers: { 'Content-Type': 'application/json' } });
+    this.setState({loggedIn:true})
+    console.log(response);
+    ReactDOM.render(<App />, document.getElementById('root'));
   }
+
   renderLoggedIn(){
     return (
       <div className="App">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="Container-fluid">
-              <div className="navbar-header">
-                <a href="" className="navbar-brand">What is this??</a>
-              </div>
-            </div>
-          </nav>     
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="#home">React-Bootstrap</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav>
+            <NavItem eventKey={1} href="#">
+              Link
+            </NavItem>
+            <NavItem eventKey={2} href="#">
+              Link
+            </NavItem>
+            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
+              <MenuItem eventKey={3.1}>Action</MenuItem>
+              <MenuItem eventKey={3.2}>Another action</MenuItem>
+              <MenuItem eventKey={3.3}>Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey={3.4}>Separated link</MenuItem>
+            </NavDropdown>
+          </Nav>
+        </Navbar>
         <header className="App-header">
           <h1 className="App-title">Welcome {this.state.username}! Upload a picture below!</h1>
         </header>
@@ -69,13 +106,14 @@ class App extends Component {
   }
   renderDefault(){
     return (
-      <div className="App">  
+      <div className="App"> 
        <MuiThemeProvider>
         <header className="App-header">
           <h1 className="App-title">Welcome to "What is this?"</h1>
         </header>
+        <br/>
         <header className="Prompt">
-          <h2 className="Login">Please login below!</h2>
+          <h2 className="Login">Please login/register below!</h2>
         </header>
          <TextField
              hintText="Enter your Username"
@@ -90,7 +128,10 @@ class App extends Component {
                onChange = {(event,newValue) => this.setState({password:newValue})} 
               />
               <br/>
-             <RaisedButton label="Submit" primary={true} onClick={(event) => this.handleLogin(event)}/>
+              <br/>
+             <RaisedButton label="Login" primary={true} onClick={(event) => this.handleLogin(event)}/>
+              &nbsp; &nbsp; &nbsp; or &nbsp; &nbsp; &nbsp;
+             <RaisedButton label="Register" primary={true} onClick={(event) => this.handleRegister(event)}/>
         </MuiThemeProvider>
       </div>
     );
--- web/client/src/index.js
+++ web/client/src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
-import ImageUploader from 'react-image-uploader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
