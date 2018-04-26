d03848fb5458337cbe18aeab9be51009a3a6f988
Joel Kreider <kreiderj@purdue.edu>
2018-04-26 06:24:06 -0400

    event handlers

--- web/client/src/App.js
+++ web/client/src/App.js
@@ -6,35 +6,71 @@ import axios from 'axios';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import Nav from 'react-bootstrap/lib/Nav';
-import Navbar from 'react-bootstrap/lib/Navbar';
-import MenuItem from 'react-bootstrap/lib/MenuItem';
-import NavItem from 'react-bootstrap/lib/NavItem';
-import NavDropdown from 'react-bootstrap/lib/NavDropdown';
+import Dropzone from 'react-dropzone';
 
+//literally all this for navbar
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem } from 'reactstrap';
 
 class App extends Component {
 
   constructor(props) {
         super(props);
            this.state = {
-            loggedIn : false,
+            loggedIn : true,
             username : '',
             password : '',
             api_key  : '',
+            errors : '',
            }
-           this.onDrop = this.onDrop.bind(this);
+
+           //render functions
            this.renderLoggedIn = this.renderLoggedIn.bind(this);
            this.renderDefault = this.renderDefault.bind(this);
+
+           //event handlers
+           this.onDrop = this.onDrop.bind(this);
            this.handleLogin = this.handleLogin.bind(this);
            this.handleRegister = this.handleRegister.bind(this);
+           this.handlePostClassification = this.handlePostClassification.bind(this);
+           this.handleLogout = this.handleLogout.bind(this);
+           this.handleListAllClassifications = this.handleListAllClassifications.bind(this);
+           this.handleListUserClassifications = this.handleListUserClassifications.bind(this);
+           this.handleClassificationInfo = this.handleClassificationInfo.bind(this);
   }
 
-   onDrop(picture) {
+   onDrop(files) {
         this.setState({
-            pictures: this.state.picture.set(picture),
+            uploadedFile: files[0]
         });
     }
+
+  handlePostClassification(event){
+    const file = this.uploadedFile;
+    var apiBaseUrl = "http:/167.99.228.85:8000/api/user/classification/create/";
+    if(this.loggedIn === true){
+      var headers = {
+        'Content-Type' : 'application/json',
+        'Authorization': 'ApiKey ' + this.username + ':' + this.api_key
+      }
+      var parameters = {
+        'photo' : this.uploadedFile,
+      }
+      const response = axios.post(apiBaseUrl, JSON.stringify(parameters), headers);
+
+    }
+  }
+
   handleLogin(event){
     var apiBaseUrl = "http://167.99.228.85:8000/api/user/login/";
     var self = this;
@@ -42,9 +78,23 @@ class App extends Component {
       "username" : self.state.username,
       "password" : self.state.password
     }
-    const response = axios.post(apiBaseUrl, JSON.stringify(parameters), { headers: { 'Content-Type': 'application/json' } });
-    console.log(response);
-    this.setState({loggedIn:true})
+    const response = axios.post(apiBaseUrl, JSON.stringify(parameters), { headers: { 'Content-Type': 'application/json' } }).catch(function(error){
+      if(!error.status){
+            self.setState({errors : "Something went wrong. Please try again."});
+      }
+    });
+    console.log(response.data);
+    if(response.data !== undefined){
+      if(response.data.success === "failure"){
+        this.setState({ errors : response.data.message });
+      }else{
+        this.setState({ 
+            loggedIn : true,
+            api_key : response.data.api_key,
+            username : response.data.username,
+        });
+      }
+    }
     ReactDOM.render(<App />, document.getElementById('root'));
   }
 
@@ -56,54 +106,48 @@ class App extends Component {
       "password" : self.state.password
     }
   
-    const response = axios.post(apiBaseUrl, JSON.stringify(parameters), { headers: { 'Content-Type': 'application/json' } });
-    this.setState({loggedIn:true})
-    console.log(response);
+    const response = axios.post(apiBaseUrl, JSON.stringify(parameters), { headers: { 'Content-Type': 'application/json' } }).catch(function(error){
+      if(!error.status){
+            this.setState({errors : "Something went wrong. Please try again."});
+      }
+    });
+    console.log(response.data);
+    if(response.data !== undefined){
+      if(response.data.success !== "failure"){
+        this.setState({ errors : response.data.message });
+      }else{
+        this.setState({ 
+            loggedIn : true,
+            api_key : response.data.api_key,
+            username : response.data.username,
+        });
+      }
+    }
+    console.log(response.data);
     ReactDOM.render(<App />, document.getElementById('root'));
   }
 
   renderLoggedIn(){
     return (
-      <div className="App">
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#home">React-Bootstrap</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Link
-            </NavItem>
-            <NavItem eventKey={2} href="#">
-              Link
-            </NavItem>
-            <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-              <MenuItem eventKey={3.1}>Action</MenuItem>
-              <MenuItem eventKey={3.2}>Another action</MenuItem>
-              <MenuItem eventKey={3.3}>Something else here</MenuItem>
-              <MenuItem divider />
-              <MenuItem eventKey={3.4}>Separated link</MenuItem>
-            </NavDropdown>
-          </Nav>
-        </Navbar>
-        <header className="App-header">
-          <h1 className="App-title">Welcome {this.state.username}! Upload a picture below!</h1>
-        </header>
-        <ImageUploader
-                className="center"
-                withIcon={true}
-                withPreview={true}
-                buttonClassName="btn btn-primary"
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                maxFileSize={5242880} />
-            
-        <button className="btn btn-primary">Classify Image</button>
+      <div>
+          <div className="App">
+            <header className="App-header">
+              <h1 className="App-title">Welcome {this.state.username}! Upload a picture below!</h1>
+            </header>
+            <Dropzone
+              multiple={false}
+              accept="image/*"
+              onDrop={this.onDrop}>
+              <p>Drop an image or click to select a file to upload.</p>
+            </Dropzone>
+            <MuiThemeProvider>    
+              <RaisedButton label="Classify!" primary={true} onClick={(event) => this.handlePostClassification(event)}/>
+            </MuiThemeProvider>
+          </div>
       </div>
     );
   }
+
   renderDefault(){
     return (
       <div className="App"> 
@@ -133,9 +177,13 @@ class App extends Component {
               &nbsp; &nbsp; &nbsp; or &nbsp; &nbsp; &nbsp;
              <RaisedButton label="Register" primary={true} onClick={(event) => this.handleRegister(event)}/>
         </MuiThemeProvider>
+        <br/>
+        <br/>
+        <span class="label label-danger">{this.state.errors}</span>
       </div>
     );
   }
+
   render() {
     if(this.state.loggedIn === true){
       return this.renderLoggedIn();
